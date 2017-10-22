c8ef37518b2868269dfba31998854b261c20316c
victorjohnsonk <mail.victorjohnson@gmail.com>
2017-10-22 16:46:50 +0100

    refactor constants and defaults into config.py for cleaner modularity

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# --- Data generation ---
+NUM_CUSTOMERS = 300
+RANDOM_STATE = 42
+DATA_PATH = "data/synthetic_customers.csv"
+
+# --- Clustering ---
+N_CLUSTERS_DEFAULT = 5
+
+# --- Visualization ---
+HTML_OUTPUT = "data/customer_clusters.html"
+PLOT_TITLE = "Customer Segmentation (PCA Projection)"
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from src.data_generator import generate_customer_data, save_dataset
 from src.clustering import run_kmeans
 from src.visualization import apply_pca, plot_clusters
 import argparse
+from src import config
+
 
 def main():
     parser = argparse.ArgumentParser(description="Customer Segmentation Tool")
@@ -11,14 +13,19 @@ def main():
     args = parser.parse_args()
     print(f"Starting Pipeline with {args.clusters} clusters...")
     # generate data
-    df = generate_customer_data(num_customers=300)
+    df = generate_customer_data(num_customers=config.NUM_CUSTOMERS, random_state=config.RANDOM_STATE)
     save_dataset(df)
+    clustered_df, model = run_kmeans(df, n_clusters=args.clusters)
     # gun K-Means clustering
     clustered_df, model = run_kmeans(df, n_clusters=args.clusters)
     print("✅ Clustering completed.")
     # apply PCA for visualization
     reduced_df, pca = apply_pca(clustered_df)
     # generate interactive scatter plot
-    html_path = f"data/customer_clusters_{args.clusters}.html"
-    plot_clusters(reduced_df, save_html=html_path)
+    html_path = config.HTML_OUTPUT.replace(".html", f"_{args.clusters}.html")
+    plot_clusters(reduced_df, title=config.PLOT_TITLE, save_html=html_path)
     print(f"Pipeline complete! Output saved in '{html_path}'")
+
+
+if __name__ == "__main__":
+    main()
